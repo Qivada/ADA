9216e44f7103d605873798a6b5d6f279acaf2c51
Ari Rämö <109618668+AriRamo@users.noreply.github.com>
2025-02-04 14:27:58 +0200

    Update configuration-table.sql

--- AzureDeployment/microsoft-fabric/library/bronze/configuration-table.sql
+++ AzureDeployment/microsoft-fabric/library/bronze/configuration-table.sql
@@ -5,3 +5,21 @@ CREATE TABLE [Configuration_Bronze].[name-of-source-system].[bronze]
 	[Dataset] [varchar](250)  NULL,
 	[Tag] [varchar](250)  NULL
 )
+
+/*
+-- Data pipeline lookup usage example
+SELECT  DISTINCT 
+        T1.[Query], 
+        LOWER(T1.[Dataset]) AS [Dataset]
+FROM    [adventureWorksLT].[bronze] AS T1
+        CROSS APPLY STRING_SPLIT(T1.Tag, ',') AS T2
+WHERE   T1.[IsActive] = 1 AND
+        (
+            LOWER(TRIM(T2.[value])) IN (
+                SELECT  LOWER(TRIM(value)) AS Tag
+                FROM    STRING_SPLIT('@{pipeline().parameters.Tag}', ',')
+            ) OR
+            TRIM('@{pipeline().parameters.Tag}') = '' OR
+            TRIM('@{pipeline().parameters.Tag}') = '*'
+        )
+*/
