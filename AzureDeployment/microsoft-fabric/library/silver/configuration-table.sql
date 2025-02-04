4ff8088f37d22dad160b021830e44e79229d2dc8
Ari Rämö <109618668+AriRamo@users.noreply.github.com>
2025-02-04 14:27:01 +0200

    Update configuration-table.sql

--- AzureDeployment/microsoft-fabric/library/silver/configuration-table.sql
+++ AzureDeployment/microsoft-fabric/library/silver/configuration-table.sql
@@ -12,3 +12,28 @@ CREATE TABLE [Configuration_Silver].[name-of-source-system].[silver]
 	[DeleteFilterColumns] [varchar](250)  NULL,
 	[Tag] [varchar](8000)  NULL
 )
+
+/*
+-- Data pipeline lookup usage example
+SELECT DISTINCT 
+       ISNULL(T1.[Type], '') AS [Type],
+       ISNULL(T1.[BronzeSchema], '') AS [BronzeSchema],
+       ISNULL(T1.[BronzeTable], '') AS [BronzeTable],
+       ISNULL(T1.[SilverSchema], '') AS [SilverSchema],
+       ISNULL(T1.[SilverTable], '') AS [SilverTable],    
+       ISNULL(T1.[BusinessKeyColumns], '') AS [BusinessKeyColumns],
+       ISNULL(T1.[ExtractColumns], '') AS [ExtractColumns],
+       ISNULL(T1.[ExcludeColumns], '') AS [ExcludeColumns],
+       ISNULL(T1.[DeleteFilterColumns], '') AS [DeleteFilterColumns]
+FROM   [adventureWorksLT].[silver] AS T1
+       CROSS APPLY STRING_SPLIT(T1.Tag, ',') AS T2
+WHERE  T1.[IsActive] = 1 AND
+        (
+            LOWER(TRIM(T2.[value])) IN (
+                SELECT  LOWER(TRIM(value)) AS Tag
+                FROM    STRING_SPLIT('@{pipeline().parameters.Tag}', ',')
+            ) OR
+            TRIM('@{pipeline().parameters.Tag}') = '' OR
+            TRIM('@{pipeline().parameters.Tag}') = '*'
+        )
+*/
