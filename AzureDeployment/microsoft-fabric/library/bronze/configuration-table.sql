b1d455cc083aa57e93aeb485f92279d56bc2cd6e
Ari Rämö <109618668+AriRamo@users.noreply.github.com>
2025-02-04 14:23:49 +0200

    Create configuration-table.sql

---
+++ AzureDeployment/microsoft-fabric/library/bronze/configuration-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [Configuration_Bronze].[name-of-source-system].[bronze]
+(
+	[IsActive] [bit]  NULL,
+	[Query] [varchar](8000)  NULL,
+	[Dataset] [varchar](250)  NULL,
+	[Tag] [varchar](250)  NULL
+)
